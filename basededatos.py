9c497d4e58040c354887ff57db6923b170a5820f
JuanPZ2000 <juanpablozuluaga2000@hotmail.com>
2021-11-16 19:59:38 -0500

    entrega final

---
+++ basededatos.py
@@ -0,0 +1,79 @@
+import dlib
+import cv2
+import numpy as np
+import os
+import sys
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def lectura(path, N):
+    images = []
+    for i in range(N):
+        image_name = "image" + str(i + 1) + ".jpeg"
+        path_file = os.path.join(path, image_name)
+        images.append(cv2.imread(path_file))
+    return images
+
+
+predictor_path = os.path.abspath(os.getcwd()) + '\shape_predictor_68_face_landmarks.dat'
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+#vid = cv2.VideoCapture(0)
+
+#while True:
+
+#ret, frame = vid.read()
+path = os.path.abspath(os.getcwd()) + '\images'
+N = len(os.listdir(path))
+images = lectura(path, N)
+distancia = []
+etiquetas = []
+
+for i in range(N):
+    frame = images[i]
+
+    new_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    dets = detector(new_gray, 1)
+    distanciaaux = []
+
+    # Etiquetas
+    if i < 50:
+        etiquetas.append(0) # Feli
+    elif i>=50and i<101:
+        etiquetas.append(1) # tiste
+    elif i >= 100 and i < 151:
+        etiquetas.append(2)  # sorpesa
+    else:
+        etiquetas.append(3)  # sorpesa
+
+    try:
+        if dets[0] is not None:
+            shape = predictor(new_gray, dets[0])
+            old_points = np.zeros((68, 2), dtype=np.int32)
+            for i in range(0, 68):
+                old_points[i] = (shape.part(i).x, shape.part(i).y)
+            for (x, y) in old_points:
+                distanciaaux.append(((x - old_points[0][0]) * 2 + (y - old_points[0][1]) * 2) ** 1/2)
+                cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+
+            distancia.append(distanciaaux)
+
+    except IndexError:
+        distancia.append(list(np.zeros(68)))
+        pass
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #break
+    # cv2.imshow('frame', frame)
+    # cv2.waitKey(0)
+    # AQUI ACABA EL GUAIL
+# After the loop release the cap object
+#vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+with open("obj.pickle", "wb") as f:
+    pickle.dump(distancia, f)
+print("Hecho")
--- main.py
+++ main.py
@@ -6,63 +6,37 @@ import sys
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
 
-def lectura(path, N):
-    images = []
-    for i in range(N):
-        image_name = "image" + str(i + 1) + ".jpeg"
-        path_file = os.path.join(path, image_name)
-        images.append(cv2.imread(path_file))
-    return images
 
 predictor_path = os.path.abspath(os.getcwd()) + '\shape_predictor_68_face_landmarks.dat'
-#predictor_path = 'C:/Users/usuario/Downloads/shape/shape_predictor_68_face_landmarks.dat'
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
-
-
 path = os.path.abspath(os.getcwd()) + '\images'
 N = len(os.listdir(path))
-images = lectura(path, N)
+
 distancia = []
 etiquetas = []
 
 for i in range(N):
-    frame = images[i]
-
-    new_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    dets = detector(new_gray, 1)
-    distanciaaux = []
-
-    # Etiquetas
     if i < 50:
-        etiquetas.append(0) # Feli
+        etiquetas.append(0)  # Feli
+    elif i >= 50 and i < 101:
+        etiquetas.append(1)  # tiste
+    elif i >= 100 and i < 151:
+        etiquetas.append(2)  # sorpesa
     else:
-        etiquetas.append(1) # tiste
+        etiquetas.append(3)  # enojado
 
-    try:
-        if dets[0] is not None:
-            shape = predictor(new_gray, dets[0])
-            old_points = np.zeros((68, 2), dtype=np.int32)
-            for i in range(0, 68):
-                old_points[i] = (shape.part(i).x, shape.part(i).y)
-            for (x, y) in old_points:
-                distanciaaux.append(((x - old_points[0][0]) * 2 + (y - old_points[0][1]) * 2) ** 1/2)
-                cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
-
-            distancia.append(distanciaaux)
-
-    except IndexError:
-        distancia.append(list(np.zeros(68)))
-        pass
-
-cv2.destroyAllWindows()
+with open("obj.pickle", "rb") as f:
+    distancia = pickle.load(f)
+print(distancia)
 
 #########################################################################################
 
 # ML
-X_train, X_test, y_train, y_test = train_test_split(distancia, etiquetas,test_size=0.1, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(distancia, etiquetas,test_size=0.2, random_state=0)
 # Se escalan las distancias
 scaler = StandardScaler()
 scaler.fit(X_train)
@@ -97,10 +71,20 @@ while True:
             y_predict=knn.predict(a)
             if(y_predict==0):
                 print("feliz")
+                cv2.putText(frame, "feliz", (100,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,255), 5, bottomLeftOrigin=False)
             elif(y_predict==1):
+                cv2.putText(frame, "triste", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 5,
+                            bottomLeftOrigin=False)
                 print("triste")
+            elif (y_predict == 2):
+                cv2.putText(frame, "sorpresa", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), 5,
+                            bottomLeftOrigin=False)
+                print("sorpresa")
+            elif (y_predict == 3):
+                cv2.putText(frame, "enojado", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 5,
+                            bottomLeftOrigin=False)
+                print("sorpresa")
     except IndexError:
         pass
     cv2.imshow('Video', frame)
     cv2.waitKey(1)
-cap.release()
